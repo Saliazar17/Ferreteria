7b066de713c7e118feeaa95a82a4ebcce83683f2
Saliazar17 <jsiliezarmoreno123@gmail.com>
2024-11-19 01:50:13 -0600

    edit cosas de header y cambiar fondo del hero

--- data/hero/hero.js
+++ data/hero/hero.js
@@ -1,8 +1,8 @@
 export const getHeroData = ()=>({
     hero: {
-        sImgUrl:'https://picsum.photos/id/18/1300/720',
-        mImgUrl:'https://picsum.photos/id/18/1024/640',
-        lImgUrl:'https://picsum.photos/id/18/1300/720',
+        sImgUrl:'/img/organi2.jpg',
+        mImgUrl:'/img/organi2.jpg',
+        lImgUrl:'/img/organi2.jpg',
         heroTitle:"'Ferreteria Mafermas'",
         body: '<p>Somos una Microempresa que busca ofrecer un buen servicio, materiales ferreteros demandantes y adaptados a las necesidades de nuestros clientes.</p>'
     }
